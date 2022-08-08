87e428eee022656b2fdfd64e649ddba10289b39e
Erivandersonluiz <110233381+Erivandersonluiz@users.noreply.github.com>
2022-08-08 19:58:15 -0300

    Update operacao_matematicas.py

--- operacao_matematicas.py
+++ operacao_matematicas.py
@@ -1,15 +1,21 @@
+import logging
 
-
+logging.basicConfig(filename='operacao_matematicas.log', 
+                    level=logging.DEBUG, 
+                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
 
 def soma(*args):
     s = sum(args)
-    print(f'suma {args} = {s}')
+    #print(f'soma {args} = {s}')
+    logging.debug(f'soma {args} = {s}')
     return s
 
 def media(*args):
-    m = sum(args)
-    print(f'media {args} = {m}')
+    m = sum(args)/len(args)
+    #print(f'media {args} = {m}')
+    logging.debug(f'media {args} = {m}')
+
     return m 
 
 def divisao(*args):
@@ -18,7 +24,9 @@ def divisao(*args):
     for ab in args:
         for a,b in ab:
             resultados.append(a/b)
-    print(f'divisao {args} = {resultados}')
+    #print(f'divisao {args} = {resultados}')
+    logging.debug(f'divisao {args} = {resultados}')
+
     return resultados
 
 soma(23,85,41,10) 
